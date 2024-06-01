d1abaef69bc2d40be332cc49b68caeae75c2ca8a
omor777 <omor0843@gmail.com>
2024-06-01 18:17:06 +0600

    added a post route for managers collection

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors());
@@ -26,12 +26,38 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
 
     const employeeCollection = client.db("assetDB").collection("employees");
+    const managerCollection = client.db("assetDB").collection("managers");
 
     // employee related api
     app.post("/employees", async (req, res) => {
       const employee = req.body;
       const result = await employeeCollection.insertOne(employee);
       res.send(result);
+    });
+
+    // manager related api
+    app.post("/managers", async (req, res) => {
+      const hrDAta = req.body;
+      const result = await managerCollection.insertOne(hrDAta);
+      res.send(result);
+    });
+
+    // create payment intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const priceInCent = parseFloat(price) * 100;
+      console.log(priceInCent, "--------------> price");
+      if (!price || priceInCent < 1) return;
+      // generate client secrete
+      const { client_secret } = await stripe.paymentIntents.create({
+        amount: priceInCent,
+        currency: "usd",
+        automatic_payment_methods: {
+          enabled: true,
+        },
+      });
+      // send client secret as response
+      res.send({ clientSecret: client_secret });
     });
 
     // await client.connect();
